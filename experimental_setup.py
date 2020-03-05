6b736b27821a8d3d5412c6053bcdb55e4343bc47
vhirtham <volker.hirthammer@bam.de>
2020-03-05 17:03:03 +0100

    Continue cleaning up

--- experimental_setup.py
+++ experimental_setup.py
@@ -1,7 +1,16 @@
+
+
 import weldx.transformations as tf
 import weldx.utility as ut
 
+
 import numpy as np
+from scipy.spatial.transform import Rotation as rot
+
+
+def create_lcs(angles,coordinates):
+    orientation = rot.from_euler("xyz", angles, degrees=True).as_matrix()
+    return tf.LocalCoordinateSystem(orientation, coordinates)
 
 def get_setup(idx):
     setup = lambda:0
@@ -15,11 +24,16 @@ def get_setup(idx):
         setup.offset_oy_ref = ut.to_float_array([69.149, 1366.014, -131.962])
         setup.groove_angle_start = 40
         setup.groove_angle_end = 60
-        setup.orientation_sp_in_ref = np.matmul(tf.rotation_matrix_y(np.pi / 2),tf.rotation_matrix_z(np.pi / 2))
-        setup.coordinates_sp_in_ref = [-20, -10, -8]
+        list_coordinates_temp_in_ref = [[-9.96, 89.47, -0.489]]
+        list_coordinates_temp_in_ref += [[10.92, 89.97, -0.27]]
+        list_coordinates_temp_in_ref += [[-8.5, 255.6, -0.2]]
+        
         setup.coordinates_temp_1_in_base = [-9.96, 89.47, -0.489]
         setup.coordinates_temp_2_in_base = [10.92, 89.97, -0.27]
         setup.coordinates_temp_3_in_base = [-8.5, 255.6, -0.2]
+        
+        orientation_sp_in_ref = np.matmul(tf.rotation_matrix_y(np.pi / 2),tf.rotation_matrix_z(np.pi / 2))
+        coordinates_sp_in_ref = [-20, -10, -8]
     elif idx == 5:        
         setup.system_data_index = 18204
         setup.measurement_data_id = 18202
@@ -30,19 +44,38 @@ def get_setup(idx):
         setup.offset_oy_ref = ut.to_float_array([69.149, 1366.014, -131.962])
         setup.groove_angle_start = 60
         setup.groove_angle_end = 40
-        setup.orientation_sp_in_ref = np.matmul(tf.rotation_matrix_y(np.pi / 2),tf.rotation_matrix_z(np.pi / 2))
-        setup.coordinates_sp_in_ref = [-18.9, -15, -8]
+        
+        list_coordinates_temp_in_ref = [[-8.299, 84.298, -0.019]]
+        list_coordinates_temp_in_ref += [[7.307, 85.235, -0.042]]
+        list_coordinates_temp_in_ref += [[-9.331, 233.894, -0.014]]
+        
         setup.coordinates_temp_1_in_base = [-8.299, 84.298, -0.019]
         setup.coordinates_temp_2_in_base = [7.307, 85.235, -0.042]
         setup.coordinates_temp_3_in_base = [-9.331, 233.894, -0.014]
+        
+        
+        orientation_sp_in_ref = np.matmul(tf.rotation_matrix_y(np.pi / 2),tf.rotation_matrix_z(np.pi / 2))
+        coordinates_sp_in_ref = [-18.9, -15, -8]
         
         # correct angle - reference coordinate was not perfectly orthogonal to specimen 
         offset_x = 0.42
         offset_y = setup.offset_oy_ref[1] - setup.origin_ref[1]
         angle_corr = np.arcsin(offset_x / offset_y)
         rotation_matrix = tf.rotation_matrix_z(angle_corr)
-        setup.orientation_sp_in_ref = np.matmul(rotation_matrix, setup.orientation_sp_in_ref)
+        orientation_sp_in_ref = np.matmul(rotation_matrix, orientation_sp_in_ref)
 
-    setup.coordinates_cam_in_flange = [325, 0,  427]
-    setup.angles_cam_in_flange = [82, 0, -90]
+
+    
+    # create coordinate systems
+    setup.cs_sp_in_ref = tf.LocalCoordinateSystem(orientation_sp_in_ref, coordinates_sp_in_ref)
+    setup.cs_scan_in_scanner_tcp = tf.LocalCoordinateSystem([[1, 0, 0], [0, 1, 0], [0, 0, -1]], [0, 0, 260])    
+    setup.cs_scanner_tcp_in_flange = create_lcs(angles=[21.9714, -0.7410, -89.7500], coordinates=[-47.949, -105.258, 479.690])
+    setup.cs_torch_tcp_in_flange =  create_lcs(angles=[-22.0474, 0.5659, 90.7092], coordinates=[-49.325, -0.410, 477.208])
+    setup.cs_cam_in_flange = create_lcs(angles=[82, 0, -90], coordinates=[325, 0,  427])
+    
+    setup.list_cs_temp_in_ref = []
+    for i in range(len(list_coordinates_temp_in_ref)):
+        setup.list_cs_temp_in_ref += [tf.LocalCoordinateSystem(origin=list_coordinates_temp_in_ref[i])] 
+
+    
     return setup
